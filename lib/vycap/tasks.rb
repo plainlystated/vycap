31c8dfb350c80c1c260859da0ac2424a28779b88
Patrick Schless <patrick@plainlystated.com>
2013-04-15 00:00:49 -0500

    add an .erb extension, to hint at purpose

--- lib/vycap/tasks.rb
+++ lib/vycap/tasks.rb
@@ -8,7 +8,7 @@ Capistrano::Configuration.instance.load do
         remote_file = SupplyDrop::Rsync.remote_address(vycap_user, srv.host, "/config/config.boot")
         cmd = SupplyDrop::Rsync.command(
           remote_file,
-          vycap_template_dir + "/config.#{srv}",
+          vycap_template_dir + "/config.#{srv}.erb",
           :ssh => ssh_options
         )
         srv unless system cmd
@@ -17,7 +17,7 @@ Capistrano::Configuration.instance.load do
       raise "rsync failed on #{failed_servers.inspect}" if failed_servers.any?
 
       roles[:vyatta].servers.map do |srv|
-        config_template = vycap_template_dir + "/config.#{srv}"
+        config_template = vycap_template_dir + "/config.#{srv}.erb"
 
         original = File.read(config_template)
         File.open(config_template, "w") do |file|
